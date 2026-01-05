ad5bc4a71f67b4c99564f1300a8bbef0b463656c
Ali Raza <alirazamehar0321@gmail.com>
2026-01-05 07:52:10 +0500

    Add files via upload

---
+++ toggle-screen/script.js
@@ -0,0 +1,10 @@
+﻿const input=document.querySelector("#color");
+const submit=document.querySelector("#btn");
+const body=document.body;
+
+submit.addEventListener("click",()=>{
+     const color1=input.value;
+     body.style.backgroundColor=color1;
+     
+});
+
