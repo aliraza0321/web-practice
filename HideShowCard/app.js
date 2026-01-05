d7aaa7993a99e82f2b2460e5c499563bd09d35e2
Ali Raza <alirazamehar0321@gmail.com>
2026-01-05 07:55:15 +0500

    Add files via upload

---
+++ HideShowCard/app.js
@@ -0,0 +1,10 @@
+﻿let show=document.getElementById("show");
+let hidden=document.getElementById("hidden");
+let div=document.querySelector(".div");
+show.addEventListener("click",()=>{
+    div.style.display="block";
+});
+hidden.addEventListener("click",()=>{
+    div.style.display="none";
+
+});
