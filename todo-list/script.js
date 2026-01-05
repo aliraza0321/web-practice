5db5deb2d07f3d1ea0e0f71ff73d853394e3d542
Ali Raza <alirazamehar0321@gmail.com>
2026-01-05 07:53:38 +0500

    Add files via upload

---
+++ todo-list/script.js
@@ -0,0 +1,10 @@
+﻿let btn=document.body.querySelector(".btn");
+let input=document.body.querySelector("#input");
+let list=document.body.querySelector(".list");
+btn.addEventListener("click",addData);
+const addData=(evnet)=>{
+    let value=input.innerText;
+    let newnode=document.create("li");
+    
+
+}
